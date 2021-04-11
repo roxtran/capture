8ca9dbf1a04fa8e92ca68b2cd685fd6e76f3df08
giadung <48108702+giadung@users.noreply.github.com>
2021-04-11 08:17:21 -0400

    9. React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import AboutUs from './pages/AboutUs'
 // Global Style
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
+// Router
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
