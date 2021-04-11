85246829f8b864b7d6506d5e75b20491395903c9
giadung <48108702+giadung@users.noreply.github.com>
2021-04-11 17:42:57 -0400

    21. Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,70 @@
 import React from 'react'
+import styled from 'styled-components'
 // Animation
 import { motion } from 'framer-motion'
-import { pageAnimation } from '../animation'
+import { pageAnimation, titleAnimation } from '../animation'
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial='hidden'
       animate='show'
       exit='exit'
-      style={{background: '#fff'}}
     >
-      contact us
-    </motion.div>
+      <Title>
+        <div className='hide'>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>
+        </div>
+      </Title>
+      <div className='hide'>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send us a message</h2>
+        </Social>
+      </div>
+      <div className='hide'>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send us an email</h2>
+        </Social>
+      </div>
+      <div className='hide'>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Social media</h2>
+        </Social>
+      </div>
+    </StyledContact>
   )
 }
+
+const StyledContact = styled(motion.div)`
+  background: white;
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+`
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`
 
 export default ContactUs
