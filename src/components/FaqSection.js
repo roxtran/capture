017dd7a9bf0f9a55e560892ea0c576354c3e1011
giadung <48108702+giadung@users.noreply.github.com>
2021-04-11 06:35:16 -0400

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How do I start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Recusandae sed eius illo. Totam animi voluptas, mollitia odio
+            blanditiis veniam fuga qui veritatis provident quis saepe quam.
+            Animi saepe distinctio reprehenderit!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>What products do you offer?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Recusandae sed eius illo. Totam animi voluptas, mollitia odio
+            blanditiis veniam fuga qui veritatis provident quis saepe quam.
+            Animi saepe distinctio reprehenderit!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Different Payment Methods</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Recusandae sed eius illo. Totam animi voluptas, mollitia odio
+            blanditiis veniam fuga qui veritatis provident quis saepe quam.
+            Animi saepe distinctio reprehenderit!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Daily Schedule</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Recusandae sed eius illo. Totam animi voluptas, mollitia odio
+            blanditiis veniam fuga qui veritatis provident quis saepe quam.
+            Animi saepe distinctio reprehenderit!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection
